63d01990b1f99c6262d50ffb02523c19654d06d3
yasiqb89 <mian.yassir@gmail.com>
2025-12-11 17:48:04 +0100

    feat: implement data-driven login tests and centralize invalid user credentials.

--- tests/e2e/login.spec.ts
+++ tests/e2e/login.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from "../../fixtures/baseFixtures";
 import { LoginPage } from "../../pages/LoginPage";
 import users from "../../data/users.json";
+import loginData from "../../data/login-data.json";
 
 
 test.describe('Login Tests', () => {
@@ -15,9 +16,29 @@ test.describe('Login Tests', () => {
     test('unsuccessfull login', { tag: ['@smoke'] }, async ({ loggedInPage }) => {
         const loginPage = new LoginPage(loggedInPage);
 
-        await loginPage.login('wrong_user', 'wrong_password');
+        await loginPage.login(users.invalidUser.username, users.invalidUser.password);
 
         const error = await loginPage.getErrorMessage();
         expect(error).toContain('Username and password do not match');
     })
 })
+
+
+// Data Driven Testing
+test.describe('Login Tests DDT', () => {
+
+    for (const data of loginData) {
+        test(data.name, { tag: ['@smoke'] }, async ({ loggedInPage }) => {
+            const loginPage = new LoginPage(loggedInPage);
+
+            await loginPage.login(data.username, data.password);
+
+            if (data.expectSuccess) {
+                await expect(loggedInPage).toHaveURL('/inventory.html');
+            } else {
+                const error = await loginPage.getErrorMessage();
+                expect(error).toContain(data.expectedError);
+            }
+        });
+    }
+});
